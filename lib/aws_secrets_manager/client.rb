059290e0d26a054cf96bdbd0356ae528b2402364
Aleksandar Popovic <aleksandar.popovic@linux.com>
2024-09-06 20:48:47 +0200

    1.0.9

--- lib/aws_secrets_manager/client.rb
+++ lib/aws_secrets_manager/client.rb
@@ -6,8 +6,6 @@ module AwsSecretsManager
       Aws::SecretsManager::Client.new(
         region: AwsSecretsManager.config.aws_region,
       )
-    rescue Errors::InvalidRegionError, Aws::Errors::InvalidRegionError
-      raise Errors.aws_region_error
     end
   end
 end
--- lib/aws_secrets_manager/errors.rb
+++ lib/aws_secrets_manager/errors.rb
@@ -13,10 +13,6 @@ module AwsSecretsManager
         Error.new("Only #{Config::PLAINTEXT} && #{Config::KEY_VALUE} secrets types is allowed!!!")
       end
 
-      def aws_region_error
-        Error.new("Please provide aws_region value in configuration object")
-      end
-
       def secret_not_exists_error(secret_name)
         Error.new("AWS Secrets Manager Secret #{secret_name} not exists!!!")
       end
